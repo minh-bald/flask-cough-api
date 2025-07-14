55958ac3f6c86e6041822211c261ad2b754faae4
Viet Ha <10423035@student.vgu.edu.vn>
2025-07-14 21:13:26 +0700

    pls work

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ output_details = interpreter.get_output_details()
 expected_length = input_details[0]['shape'][1]
 
 # Labels for 4 classes
-labels = ["Anomaly", "COVID-19", "Non-Cough", "Healthy"]
+labels = ["Anomaly", "COVID-19", "Healthy", "Non-cough"]
 
 # HTML template (no percentage display)
 HTML_TEMPLATE = """
